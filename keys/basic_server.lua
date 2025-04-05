c640b61deb58161d442dfd6eeba79b6f1b216bac
JabDoesThings <JabDoesThings@github.com>
2025-04-05 13:58:13 -0500

    Added documentation in `keys/basic_server.lua`.

--- keys/basic_server.lua
+++ keys/basic_server.lua
@@ -1,3 +1,11 @@
+---[[
+--- This file is for EtherHammerX.
+---
+--- This creates the key-fragment for the server-side of EtherHammerX operations.
+---
+--- @author asledgehammer, JabDoesThings 2025
+---]]
+
 --- @type fun(minChars: number, maxChars?: number): string
 local randomstring = require "asledgehammer/randomstring";
 

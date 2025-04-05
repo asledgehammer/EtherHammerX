640573baece0520197558b0a458df9f5c563a0f3
JabDoesThings <JabDoesThings@github.com>
2025-04-05 16:44:46 -0500

    Updates to EtherHack module.

--- modules/etherhack_client.lua
+++ modules/etherhack_client.lua
@@ -45,16 +45,40 @@ local etherHackAPIClasses = {
     'EtherPlayerEditor',
     'EtherSettingsPanel',
     'EtherVisualsPanel',
+    -- 'ISUIElement' -- [DEBUG]
 };
 
 --- @type EtherHammerXClientModule
 local module = function(api, options)
-    local classes = api.getGlobalClasses();
+
+    local function classExists()
+        local classes = api.getGlobalClasses();
+        for _, class in ipairs(classes) do
+            for _, className in ipairs(etherHackAPIClasses) do
+                if class.globalName == className or class.typeName == className then
+                    return true;
+                end
+            end
+        end
+        return false;
+    end
+
     local funcs = api.getGlobalFunctions();
-    if api.anyExists(funcs, etherHackFunctions) or api.anyExists(classes, etherHackAPIClasses) then
+    if api.anyExists(funcs, etherHackFunctions) then
+        print('EtherHack: Injected Function detected.');
         api.report('EtherHack', 'Injected function', false);
         if options.SUBMIT_TICKET_ON_KICK then
-            api.submitTicket('Hello, I am using EtherHack, detected by EtherHammerX.', function()
+            api.submitTicket('Hello, I am using EtherHack, detected by EtherHammerX. (Code Injection)', function()
+                api.disconnect();
+            end);
+        else
+            api.disconnect();
+        end
+    elseif classExists() then
+        print('EtherHack: Injected Class detected.');
+        api.report('EtherHack', 'Injected class', false);
+        if options.SUBMIT_TICKET_ON_KICK then
+            api.submitTicket('Hello, I am using EtherHack, detected by EtherHammerX. (Code Injection)', function()
                 api.disconnect();
             end);
         else
